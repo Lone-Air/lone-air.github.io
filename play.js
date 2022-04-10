a0c6f5bce3d41d256079ed5619a404f1a15d6892
Lone-Air <Lone_air_Use@outlook.com>
2022-04-10 16:53:27 +0800

    -

--- play.js
+++ play.js
@@ -12,7 +12,7 @@ function playMus(arr){
     mus.play()
     mus.loop=false;
     function playEndedHandler(){
-        if(repeat>=arr.length){playMus(oldarr);}
+        if(repeat>=arr.length){repeat=0;playMus(oldarr);repeat=0;return 0;}
         src=arr.pop();
         mus.src=src;
         arr.unshift(src);
